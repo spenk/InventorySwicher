a6158dd82158abe152faa6c938903ea8eafc21b6
spenk <spenko@hotmail.nl>
2012-06-01 22:00:59 +0200

    added some changes <still not workin!>

--- InventorySwicher/src/InventorySwicherListener.java
+++ InventorySwicher/src/InventorySwicherListener.java
@@ -1,12 +1,13 @@
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
 
 
 public class InventorySwicherListener extends PluginListener{
 	 static Logger log = Logger.getLogger("Minecraft");
-	
+	HashMap<String,World> worlds = new HashMap<String,World>();
 	 
 	 public void onLogin(Player player){
 		 File f = new File("plugins/config/InventorySwicher/"+player.getName()+".properties");
@@ -14,13 +15,22 @@ public class InventorySwicherListener extends PluginListener{
 		 PropertiesFile f1 = new PropertiesFile("plugins/config/InventorySwicher/"+player.getName()+".properties");
 		 f1.save();
 		 }
+			if (worlds.containsKey(player.getName())){
+				worlds.remove(player.getName());
+				 worlds.put(player.getName(), player.getWorld());
+			}else{
+		 worlds.put(player.getName(), player.getWorld());
+			}
 	 }
 	public boolean onTeleport(Player player,Location from,Location to){
+		if (to.getWorld() == worlds.get(player.getName())){return false;}else{
+			worlds.remove(player.getName()); worlds.put(player.getName(), to.getWorld());
 			PropertiesFile file = new PropertiesFile("plugins/config/InventorySwicher/"+player.getName()+".properties");
 			saveinv(player,from.getWorld().getName());
 			log.info("looping to inventory saver..");
 			if (file.containsKey(to.getWorld().getName())){
 				setinv(player,to.getWorld().getName());
+		}
 		}
 		return false;
 }
@@ -55,16 +65,20 @@ public class InventorySwicherListener extends PluginListener{
 	}
 	
 	public void saveinv(Player player,String world){
+		StringBuilder sb = new StringBuilder();
 		log.info("[InventorySwicher] - "+player.getName()+" Inventory saving...");
 		 PropertiesFile f = new PropertiesFile("plugins/config/InventorySwicher/"+player.getName()+".properties");
 		 int i = 0;
-		 String s = "";
 		 Inventory inv = player.getInventory();
-		 while (i != 35){
+		 while (i < 35){
 			 Item item = inv.getItemFromSlot(i);
 			 if (item != null){
 				 String add = item.getItemId()+","+item.getAmount()+","+item.getDamage()+","+i;
-				 s = s+":"+add;
+				 if (sb.toString().equals("")){
+				  sb.append(add);
+				 }else{
+					 sb.append(":"+add);
+				 }
 				 inv.removeItem(i);
 				 i++;
 			 }
@@ -73,28 +87,44 @@ public class InventorySwicherListener extends PluginListener{
 		 Item ar1 = inv.getItemFromSlot(100);
 		 if (ar1 != null){
 			 String add = ar1.getItemId()+","+ar1.getAmount()+","+ar1.getDamage()+","+100;
-			 s = s+":"+add;
+			 if (sb.toString().equals("")){
+				  sb.append(add);
+				 }else{
+					 sb.append(":"+add);
+				 }
 			 inv.removeItem(100);
 		 }
 		 Item ar2 = inv.getItemFromSlot(101);
 		 if (ar2 != null){
 			 String add = ar2.getItemId()+","+ar2.getAmount()+","+ar2.getDamage()+","+101;
-			 s = s+":"+add;
+			 if (sb.toString().equals("")){
+				  sb.append(add);
+				 }else{
+					 sb.append(":"+add);
+				 }
 			 inv.removeItem(101);
 		 }
 		 Item ar3 = inv.getItemFromSlot(102);
 		 if (ar3 != null){
 			 String add = ar3.getItemId()+","+ar3.getAmount()+","+ar3.getDamage()+","+102;
-			 s = s+":"+add;
+			 if (sb.toString().equals("")){
+				  sb.append(add);
+				 }else{
+					 sb.append(":"+add);
+				 }
 			 inv.removeItem(102);
 		 }
 		 Item ar4 = inv.getItemFromSlot(102);
 		 if (ar4 != null){
 			 String add = ar4.getItemId()+","+ar4.getAmount()+","+ar4.getDamage()+","+103;
-			 s = s+":"+add;
+			 if (sb.toString().equals("")){
+				  sb.append(add);
+				 }else{
+					 sb.append(":"+add);
+				 }
 			 inv.removeItem(103);
 		 }
-		 f.setString(world, s);
+		 f.setString(world, sb.toString());
 		 log.info("[InventorySwicher] - inventory saved...");
 	}
 	
